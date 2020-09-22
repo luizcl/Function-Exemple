a9d0fa6078c464f3da5188c52671700783297cc2
Luiz <luiz.cl@live.com>
2020-09-22 12:41:11 -0300

    With new Classe implementation - using map method

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.UK);
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("Tablet", 250.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		// map --> Apply to every element in the list
+		List<String> name = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		name.forEach(System.out::println);
+		
+	}
+	
+}
