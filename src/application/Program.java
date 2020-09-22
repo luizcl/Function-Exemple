d7c167d5ab04a57ace0ac05acc3128f4dc254028
Luiz <luiz.cl@live.com>
2020-09-22 12:47:43 -0300

    With a declared lambda function

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -20,8 +21,10 @@ public class Program {
 		list.add(new Product("Tablet", 250.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		Function<Product , String> func = p -> p.getName().toUpperCase();
+		
 		// map --> Apply to every element in the list
-		List<String> name = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		List<String> name = list.stream().map(func).collect(Collectors.toList());
 		
 		name.forEach(System.out::println);
 		
